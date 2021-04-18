afbff7a673dbb320f47b508f58db186ff25a4564
wisdom08 <wisdom08@kakao.com>
2021-04-18 11:15:08 +0900

    뒤집은 소수

---
+++ src/Array/Q06.java
@@ -0,0 +1,58 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//뒤집은 소수
+public class Q06 {
+
+
+    public static void main(String[] args) {
+        Q06 T = new Q06();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+
+        }
+    }
+
+    private static ArrayList<Integer> solution(int n, int[] arr) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+            if (isPrime(res)) {
+                answer.add(res);
+            }
+        }
+
+        return answer;
+    }
+
+    private static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
